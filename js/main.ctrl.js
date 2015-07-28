fadb7d25058ae1aa195cdf64bebed15b4a175eb9
Kostas Agorastidis <kagorastidis@gmail.com>
2015-07-28 12:47:07 +0300

    Added files

---
+++ js/main.ctrl.js
@@ -0,0 +1,85 @@
+angular.module('app').controller("MainController", function($scope){
+    var vm = this;
+    $scope.maxItems=6;
+     vm.cats = [
+        {   image:'img/cats/cynthia.jpg',
+            name: 'Cynthia',
+            location: 'Lincoln Park'
+        },
+
+        {   image:'img/cats/grenada.jpg',
+            name: 'Grenada',
+            location: "Resque & Recovery Center"
+        },
+
+        {   image:'img/cats/kona.jpg',
+            name: 'Kona',
+            location: 'Lincoln Park'
+        },
+        {   
+        	image:'img/cats/jewel.jpg',
+            name: 'Jewel',
+            location: 'Lincoln Park'
+        },
+
+         {  image:'img/cats/cynthia.jpg',
+            name: 'Cynthia',
+            location: 'Lincoln Park'
+        },
+
+        {   image:'img/cats/grenada.jpg',
+            name: 'Grenada',
+            location: "Resque & Recovery Center"
+        },
+
+        {   image:'img/cats/kona.jpg',
+            name: 'Kona',
+            location: 'Lincoln Park'
+        },
+        {   
+        	image:'img/cats/jewel.jpg',
+            name: 'Jewel',
+            location: 'Lincoln Park'
+        },
+          {   image:'img/cats/cynthia.jpg',
+            name: 'Cynthia',
+            location: 'Lincoln Park'
+        },
+
+        {   image:'img/cats/grenada.jpg',
+            name: 'Grenada',
+            location: "Resque & Recovery Center"
+        },
+
+        {   image:'img/cats/kona.jpg',
+            name: 'Kona',
+            location: 'Lincoln Park'
+        },
+        {   
+        	image:'img/cats/jewel.jpg',
+            name: 'Jewel',
+            location: 'Lincoln Park'
+        },
+          {   image:'img/cats/cynthia.jpg',
+            name: 'Cynthia',
+            location: 'Lincoln Park'
+        },
+
+        {   image:'img/cats/grenada.jpg',
+            name: 'Grenada',
+            location: "Resque & Recovery Center"
+        },
+
+        {   image:'img/cats/kona.jpg',
+            name: 'Kona',
+            location: 'Lincoln Park'
+        },
+        {   
+        	image:'img/cats/jewel.jpg',
+            name: 'Jewel',
+            location: 'Lincoln Park'
+        }
+       ];
+
+
+});
